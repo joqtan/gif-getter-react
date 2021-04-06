63fa735506aa221d3e9a229f4d9bd75aa0f418d6
joqtan <joqtanmarmol@gmail.com>
2021-04-06 01:06:51 -0600

    test files and fixes

---
+++ src/test/GifGridItem.test.js
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme'
+import GifGridItem from '../components/GifGridItem'
+
+describe('tests on <GifGridItem/> component', () => {
+    const title = 'title'
+    const url = 'http:localhost/image.jpg'
+    const wrapper = shallow(<GifGridItem title={title} url={url} />)
+    test('should show the component properly', () => {
+        expect(wrapper).toMatchSnapshot()
+    })
+    test('p should have a paragraph with the title', () => {
+        const p = wrapper.find('p')
+        expect(p.text().trim()).toBe(title)
+    })
+    test('img should have a image with source and alt from props', () => {
+        const img = wrapper.find('img')
+        expect(img.prop('src')).toBe(url)
+        expect(img.prop('alt')).toBe(title)
+    })
+    test('div should contain a propertie called animate__fadeInUpBig', () => {
+        const div = wrapper.find('div')
+        expect(div.hasClass('animate__fadeInUpBig')).toBe(true)
+    })
+})
---
+++ src/test/getGif.test.js
@@ -0,0 +1,11 @@
+import { getGif } from '../helpers/getGifs'
+describe('tests on getGif helper', () => {
+    test('should get 5 elements', async () => {
+        const gifs = await getGif('random')
+        expect(gifs.length).toBe(5)
+    })
+    test('should receive a category', async () => {
+        const gifs = await getGif('')
+        expect(gifs.length).toBe(0)
+    })
+})
